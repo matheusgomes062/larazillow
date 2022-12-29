84b4adcbfc4b87d4aab6babbce24b2770e5e688c
Matheus Gomes <matheusgomes062@gmail.com>
2022-12-29 04:13:22 -0300

    feat: progress bar loading indicator

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,15 @@ import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import '../css/app.css'
 import MainLayout from './Layouts/MainLayout.vue'
 import { ZiggyVue } from 'ziggy'
+import { InertiaProgress } from '@inertiajs/progress'
 import '../css/app.css'
+
+InertiaProgress.init({
+  delay: 0,
+  color: '#29d',
+  includeCSS: true,
+  showSpinner: true,
+})
 
 createInertiaApp({
   resolve: async (name) => {
